f2f5cb88c719003bc677521440543562a7e38951
denis.babich.acc@gmail.com <denis.babich.acc@gmail.com>
2022-08-30 21:43:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructures
+{
+    internal sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
