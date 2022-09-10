3c251f873a3906708983fba80c5797363f2d0e61
denis.babich.acc@gmail.com <denis.babich.acc@gmail.com>
2022-09-10 19:23:57 +0300

    Deleted code from Main()

--- Program.cs
+++ Program.cs
@@ -6,34 +6,7 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            DataStructures.Queue<string> queue = new DataStructures.Queue<string>();
-            queue.Enqueue("Denis");
-            queue.Enqueue("Vlad");
-            queue.Enqueue("Vadim");
-
-            Console.WriteLine(queue.Count);
-
-            if(queue.TryDequeue(out string result1))
-                Console.WriteLine(result1);
-
-            Console.WriteLine(queue.Count);
-
-            if (queue.TryDequeue(out string result2))
-                Console.WriteLine(result2);
-
-            Console.WriteLine(queue.Count);
-
-            if (queue.TryDequeue(out string result3))
-                Console.WriteLine(result3);
-
-            Console.WriteLine(queue.Count);
-
-            if (queue.TryDequeue(out string result4))
-                Console.WriteLine(result4);
-            else
-                Console.WriteLine("Error");
-
-            Console.WriteLine(queue.Count);
+            
         }
     }
 }
