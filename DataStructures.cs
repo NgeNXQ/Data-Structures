b6581032e51dfa9af6f9929c01d0f98f8b35aeb6
denis.babich.acc@gmail.com <denis.babich.acc@gmail.com>
2022-10-16 20:49:36 +0300

    Mnay changes

--- DataStructures.cs
+++ DataStructures.cs
@@ -342,6 +342,485 @@
     }
     #endregion
 
+    #region Doubly Linked List
+    internal sealed class DoublyLinkedList<T>
+    {
+        private DoublyLinkedListNode<T> head;
+        private DoublyLinkedListNode<T> tail;
+
+        private int count = 0;
+
+        public int Count { get => count; }
+
+        public DoublyLinkedList() { }
+
+        public DoublyLinkedList(params T[] items)
+        {
+            foreach (T item in items)
+                this.AddLast(item);
+        }
+
+        public void AddFirst(DoublyLinkedListNode<T> newNode)
+        {
+            if (newNode == null)
+                throw new System.ArgumentNullException($"{nameof(newNode)} is null.");
+
+            newNode.Next = head;
+            head = newNode;
+            ++count;
+        }
+
+        public DoublyLinkedListNode<T> AddFirst(T item)
+        {
+            DoublyLinkedListNode<T> newNode = new DoublyLinkedListNode<T>(item);
+
+            newNode.Next = head;
+            head = newNode;
+            ++count;
+
+            return newNode;
+        }
+
+        public DoublyLinkedListNode<T> AddLast(T item)
+        {
+            DoublyLinkedListNode<T> newNode = new DoublyLinkedListNode<T>(item);
+
+            if (head == null)
+                head = tail = newNode;
+            else
+            {
+                tail.Next = newNode;
+                newNode.Previous = tail;
+                tail = newNode;
+            }
+
+            ++count;
+
+            return newNode;
+        }
+
+        public void AddLast(DoublyLinkedListNode<T> newNode)
+        {
+            if (newNode == null)
+                throw new System.ArgumentNullException($"{nameof(newNode)} is null.");
+
+            if (head == null)
+                head = tail = newNode;
+            else
+            {
+                tail.Next = newNode;
+                newNode.Previous = tail;
+                tail = newNode;
+            }
+
+            ++count;
+        }
+
+        internal class DoublyLinkedListNode<T>
+        {
+            public DoublyLinkedListNode<T>? Previous { get; set; }
+
+            public DoublyLinkedListNode<T>? Next { get; set; }
+
+            public T? Value { get; set; }
+
+            public DoublyLinkedListNode() { }
+
+            public DoublyLinkedListNode(T value) { this.Value = value; }
+
+            public override string ToString() => $"{this.Value}";
+        }
+    }
+
+    internal sealed class DoublyLinkedListNode<T>
+    {
+
+    }
+    #endregion
+
+    #region Circular Linked List
+    internal sealed class CircularLinkedList<T>
+    {
+        private CircularLinkedListNode<T>? head;
+        private CircularLinkedListNode<T>? tail;
+
+        private int count;
+
+        public CircularLinkedListNode<T> First 
+        { 
+            get
+            {
+                if (count == 0)
+                    throw new System.InvalidOperationException($"{nameof(CircularLinkedList<T>)} is empty.");
+
+                return head;
+            }
+        }
+        public CircularLinkedListNode<T> Last 
+        { 
+            get
+            {
+                if (count == 0)
+                    throw new System.InvalidOperationException($"{nameof(CircularLinkedList<T>)} is empty.");
+
+                return tail;
+            }
+        }
+
+        public int Count { get => count; }
+
+        public CircularLinkedList() { }
+
+        public CircularLinkedList(params T[] items)
+        {
+            for (int i = 0,length = items.Length; i < length; ++i)
+                this.AddLast(items[i]);
+        }
+
+        public CircularLinkedListNode<T> AddFirst(T item)
+        {
+            CircularLinkedListNode<T> newNode = new CircularLinkedListNode<T>(item);
+
+            if (head == null)
+                head = tail = newNode;
+            else
+            {
+                newNode.Next = head;
+                head = newNode;
+                tail.Next = newNode;
+            }
+
+            ++count;
+            return newNode;
+        }
+
+        public void AddFirst(CircularLinkedListNode<T> node)
+        {
+            if (node == null)
+                throw new System.ArgumentNullException($"{nameof(node)} is null.");
+
+            if (head == null)
+                head = tail = node;
+            else
+            {
+                node.Next = head;
+                head = node;
+                tail.Next = head;
+            }
+
+            ++count;
+        }
+
+        public CircularLinkedListNode<T> AddLast(T item)
+        {
+            CircularLinkedListNode<T> newNode = new CircularLinkedListNode<T>(item);
+
+            if (head == null)
+                head = tail = newNode;
+            else
+            {
+                tail.Next = newNode;
+                newNode.Next = head;
+                tail = newNode;
+            }
+
+            ++count;
+            return newNode;
+        }
+
+        public void AddLast(CircularLinkedListNode<T> node)
+        {
+            if (node == null)
+                throw new System.ArgumentNullException($"{nameof(node)} is null.");
+
+            if (head == null)
+                head = tail = node;
+            else
+            {
+                tail.Next = node;
+                node.Next = head;
+            }
+
+            ++count;
+        }
+
+        //public void RemoveFirst()       //Fix this shit
+        //{
+
+        //}
+
+        //public void RemoveLast()
+        //{
+
+        //}
+
+        public bool Remove(CircularLinkedListNode<T> item)
+        {
+            if(count != 0 && item != null)
+            {
+                CircularLinkedListNode<T> current = head, previous = head;
+
+                for (int i = 0, length = count; i < length; ++i)
+                {
+                    if (current.Equals(item))
+                    {
+                        previous = current.Next;
+                        --count;
+                        return true;
+                    }
+
+                    previous = current;
+                    current = current.Next;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Remove(T item)
+        {
+            if (count != 0)
+            {
+                CircularLinkedListNode<T> current = head, previous = head;
+
+                for (int i = 0, length = count; i < length; ++i)
+                {
+                    if(current.Value.Equals(item))
+                    {
+                        previous = current.Next;
+                        --count;
+                        return true;
+                    }
+
+                    previous = current;
+                    current = current.Next;
+                }
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+    }
+
+    internal sealed class CircularLinkedListNode<T>
+    {
+        public T? Value { get; set; }
+        public CircularLinkedListNode<T>? Next { get; set; }
+
+        public CircularLinkedListNode() { }
+
+        public CircularLinkedListNode(T value) { this.Value = value; }
+    }
+    #endregion
+
+    #region Circular Doubly Linked List
+    internal sealed class CircularDoublyLinkedList<T>
+    {
+
+    }
+
+    internal sealed class CircularDoublyLinkedListNode<T>
+    {
+
+    }
+    #endregion
+
+    #region Deque
+    internal sealed class Deque<T>
+    {
+        private DequeNode<T>? head;
+        private DequeNode<T>? tail;
+
+        private int count;
+
+        public int Count { get => count; }
+
+        public bool IsEmpty { get { return count < 1 ? true : false; } }
+
+        public void PushBack(T item)
+        {
+            DequeNode<T> newNode = new DequeNode<T>(item);
+
+            if (head == null)
+                head = tail = newNode;
+            else
+            {
+                tail.Next = newNode;
+                newNode.Previous = tail;
+                tail = newNode;
+            }
+
+            ++count;
+        }
+
+        public void PushFront(T item)
+        {
+            DequeNode<T> newNode = new DequeNode<T>(item);
+
+            if (head == null)
+                head = tail = newNode;
+            else
+            {
+                head.Previous = newNode;
+                newNode.Next = head;
+                head = newNode;
+            }
+
+            ++count;
+        }
+
+        public T PopBack()
+        {
+            if (count < 1)
+                throw new System.InvalidOperationException($"{nameof(Deque<T>)} is empty. ");
+
+            T item = tail.Value;    
+            tail = tail.Previous;
+
+            if (tail == null)
+                head = null;
+
+            --count;
+            return item;
+        }
+
+        public T PopFront()
+        {
+            if (count < 1)
+                throw new System.InvalidOperationException($"{nameof(Deque<T>)} is empty.");
+
+            T item = head.Value;
+            head = head.Next;
+
+            if (head == null)
+                tail = null;
+
+            --count;
+            return item;
+        }
+
+        public bool TryPopBack(out T result)
+        {
+            if (count > 0)
+            {
+                result = tail.Value;
+                tail = tail.Previous;
+
+                if (tail == null)
+                    head = null;
+
+                --count;
+                return true;
+            }
+            else
+            {
+                result = default(T);
+                return false;
+            }
+        }
+
+        public bool TryPopFront(out T result)
+        {
+            if (count > 0)
+            {
+                result = head.Value;
+                head = head.Next;
+
+                if (head == null)
+                    tail = null;
+
+                --count;
+                return true;
+            }
+            else
+            {
+                result = default(T);
+                return false;
+            }
+        }
+
+        public T PeekBack() => count < 1 ? throw new System.InvalidOperationException($"{nameof(Deque<T>)} is empty.") : tail.Value;
+
+        public T PeekFront() => count < 1 ? throw new System.InvalidOperationException($"{nameof(Deque<T>)} is empty.") : head.Value;
+
+        public bool TryPeekBack(out T result)
+        {
+            if (count > 0)
+            {
+                result = tail.Value;
+                return true;
+            }
+            else
+            {
+                result = default(T);
+                return false;
+            }
+        }
+
+        public bool TryPeekFront(out T result)
+        {
+            if (count > 0)
+            {
+                result = head.Value;
+                return true;
+            }
+            else
+            {
+                result = default(T);
+                return false;
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            DequeNode<T> current = head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                    return true;
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        private sealed class DequeNode<T>
+        {
+            public DequeNode<T>? Previous { get; set; }
+            public DequeNode<T>? Next { get; set; }
+            public T Value { get; set; }
+
+            public DequeNode() { }
+
+            public DequeNode(T value) { this.Value = value; }
+        }
+    }
+    #endregion
+
+    #region Hash Table
+    #endregion
+
+    #region Dictionary
+    #endregion
+
+    #region Associative Array
+    #endregion
+
     #region Stack
     internal sealed class Stack<T>
     {
@@ -470,7 +949,6 @@
     #endregion
 
     #region Queue
-
     internal sealed class Queue<T>
     {
         private QueueNode<T> queue;
@@ -585,6 +1063,5 @@
             public QueueNode(T value) { this.Value = value; }  
         }
     }
-
     #endregion
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections.Generic;
-
-namespace DataStructures
+﻿namespace DataStructures
 {
     internal sealed class Program
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
